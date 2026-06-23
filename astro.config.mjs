f47bf315189ff669e64eadac42214de0002f641a
arturclementinoo <acruzclementino@gmail.comm>
2026-06-23 19:26:02 -0300

    fix: padroniza estrutura e configuracao

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
-// @ts-check
+// astro.config.mjs
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
 export default defineConfig({
+  site: 'https://arturclementino.github.io',
+  base: '/site-amigos-escolas',
   vite: {
     plugins: [tailwindcss()]
   }
